c06f6ac43e335d50a2a4a5c49a6355a56717ffc2
853041911@qq.com <jingkesi123>
2016-08-23 21:50:45 +0800

    求吸血鬼数

--- Test/My.cpp
+++ Test/My.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 #include<cstdlib>
+#include<string>
+#include<fstream>
 using namespace std;
+
 int main()
 {
-	cout << "Hello World" << endl;
+	ifstream in;
+	string s;
+	while (getline(in, s))
+		cout << s << endl;
 	system("pause");
 	return 0;
 }
